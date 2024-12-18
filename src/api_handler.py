0e1d70faf549276758b0017feb5f0e6d9e63d864
Elias <you@example.com>
2024-12-19 00:26:24 +0100

    Ajout du script d'authentification de l'API Spotify dans api_handler.py

---
+++ src/api_handler.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 19 00:20:28 2024
+
+@author: garpo
+"""
+
+import os
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+
+# Chargement des variables d'environnement
+load_dotenv()
+
+# Récupération de mes identifiants
+client_id = os.getenv('SPOTIPY_CLIENT_ID')
+client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
+
+# Authentification
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = Spotify(auth_manager=auth_manager)
+
+# Test de connexion
+results = spotify.search(q="Top Hits", limit=1)
+print("Connexion réussie ! Voici un exemple de résultat :")
+print(results)
